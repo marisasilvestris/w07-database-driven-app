848b26b19768f9b724aaefd75a09bb043e712dd9
Marisa Silvestris <marisasilvestris@gmail.com>
2026-03-02 10:04:27 +0000

    questions

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,10 +4,13 @@ import SubmitNote from "../components/SubmitNote";
 export default function HomePage() {
   return (
     <>
-      <h1 className="test">hey y'all this is a notepad</h1>
-      <p>pleas treat her gently her code is fragil as eff</p>
-      <p>y is it "not found" when you hit f5 on a non-root url</p>
-      <p>i am losing my mind</p>
+      <h1 className="test">{`hey y'all this is a notepad`}</h1>
+      <p>{`pleas treat her gently her code is fragil as eff`}</p>
+      <p>{`y is it "not found" when you hit f5 on a non-root url`}</p>
+      <p>{`y does the submitnote component not work when added to the homepage`}</p>
+      <p>{`what's with that split() problem in notes.jsx`}</p>
+      <p>{`why did all of this take me so long for such low quality`}</p>
+      <p>{`i am losing my mind`}</p>
       {/* <SubmitNote className={`w-128 h-128 max-w-screen`} /> */}
     </>
   );
